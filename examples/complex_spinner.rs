4e6a7e6b614dcb6af53fc62233832b070d34b18d
Marcel Müller <neikos@neikos.email>
2016-01-11 02:07:47 +0100

    Fix examples

--- examples/complex_spinner.rs
+++ examples/complex_spinner.rs
@@ -3,13 +3,14 @@ extern crate spinner;
 use std::time::Duration;
 use std::thread;
 
-use spinner::Spinner;
+use spinner::SpinnerBuilder;
 
 fn main() {
-    let sp = Spinner::new_custom("Long Running op!".into(), Box::new(|sp, status|{
-        format!("{spin} -- Currently working on: \'{status}\' -- {spin}",
-                spin = sp, status = status)
-    }));
+    let sp = SpinnerBuilder::new("Long Running op!".into()).
+        format(|sp, status|{
+            format!("{spin} -- Currently working on: \'{status}\' -- {spin}",
+                    spin = sp, status = status)
+        }).start();
     thread::sleep(Duration::from_millis(2000));
     sp.message("Updating...".into());
     sp.update("Fixing things...".into());
--- examples/simple_spinner.rs
+++ examples/simple_spinner.rs
@@ -3,10 +3,10 @@ extern crate spinner;
 use std::time::Duration;
 use std::thread;
 
-use spinner::Spinner;
+use spinner::SpinnerBuilder;
 
 fn main() {
-    let sp = Spinner::new("Long Running op!".into());
+    let sp = SpinnerBuilder::new("Long Running op!".into()).start();
     thread::sleep(Duration::from_millis(2000));
     sp.message("Updating...".into());
     sp.update("Fixing things...".into());
